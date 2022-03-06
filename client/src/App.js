3d04c6d28819da8acbb5efd5c52cb6c6a360b597
ho991217 <ho991217@naver.com>
2022-03-06 15:33:25 +0900

    feat: CR 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoText, setTodoText] = useState("hi");
+  const getTodos = async () => {
+    const todos_res = await axios.get(`http://localhost:8000/api/get`);
+    setTodos(todos_res.data);
+  };
+
+  const onSubmit = async () => {
+    const response = await axios.post("http://localhost:8000/api/insert", {
+      id: Date.now(),
+      completed: 0,
+      todoName: todoText,
+    });
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <button onClick={onSubmit}>check</button>
+      <button onClick={() => console.log(todos)}>check todos</button>
+    </Wrapper>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
-import express from "express";
-
-const mysql = require("mysql");
+const express = require("express");
+const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.port || 8000;
@@ -8,7 +7,7 @@ const cors = require("cors");
 
 const db = mysql.createPool({
   host: "localhost",
-  user: "root",
+  user: "ho991217",
   password: "jy219512",
   database: "todolist_db",
 });
@@ -18,4 +17,35 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // CRUD 구현?
-app.get();
+app.get("/api/get", (req, res) => {
+  const sqlQuery = "SELECT * FROM todo;";
+  db.query(sqlQuery, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.post("/api/insert", (req, res) => {
+  const id = req.body.id;
+  //   res.send("id:", id);
+  const completed = req.body.completed;
+  //   res.send("completes:", completed);
+  const todoName = req.body.todoName;
+  //   res.send("todoName:", todoName);
+  const sqlQuery =
+    "INSERT INTO todo (id, completed, todo_name) VALUES (?, ?, ?)";
+  db.query(sqlQuery, [id, completed, todoName], (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send("succesful");
+    }
+  });
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`server on port ${PORT}`);
+});
