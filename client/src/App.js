4856799f4b5544c00a487a7571eb1caccaaec924
ho991217 <ho991217@naver.com>
2022-03-06 15:36:24 +0900

    feat: CR 구현

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,13 @@ const Wrapper = styled.div`
   justify-content: center;
 `;
 
+const TodosUl = styled.ul``;
+
+const TodosLi = styled.li``;
+
 function App() {
   const [todos, setTodos] = useState([]);
-  const [todoText, setTodoText] = useState("hi");
+  const [todoText, setTodoText] = useState("nooo");
   const getTodos = async () => {
     const todos_res = await axios.get(`http://localhost:8000/api/get`);
     setTodos(todos_res.data);
@@ -37,6 +41,12 @@ function App() {
     <Wrapper>
       <button onClick={onSubmit}>check</button>
       <button onClick={() => console.log(todos)}>check todos</button>
+      <TodosUl>
+        {todos &&
+          todos.map((todo) => (
+            <TodosLi key={todo.id}>{todo.todo_name}</TodosLi>
+          ))}
+      </TodosUl>
     </Wrapper>
   );
 }
